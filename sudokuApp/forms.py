e9ce68f18d02af2da40187cb390e04aa628655ab
OffendedButAmused <45052786+OffendedButAmused@users.noreply.github.com>
2019-07-07 21:48:59 -0700

    Added error handlers to each of the flask pages

--- sudokuApp/forms.py
+++ sudokuApp/forms.py
@@ -19,37 +19,3 @@ class gridForm(FlaskForm):
     cellVals = FieldList(IntegerField(validators=[NumberRange(min=1, max=9, message=None), Optional()]), min_entries=81, max_entries=81)
     submit = SubmitField('Solve!')
 
-
-
-# class UploadForm(Form):
-#     image        = FileField(u'Image File', [validators.regexp(u'^[^/\\]\.jpg$')])
-#     description  = TextAreaField(u'Image Description')
-
-#     def validate_image(form, field):
-#         if field.data:
-#             field.data = re.sub(r'[^a-z0-9_.-]', '_', field.data)
-
-# def upload(request):
-#     form = UploadForm(request.POST)
-#     if form.image.data:
-#         image_data = request.FILES[form.image.name].read()
-#         open(os.path.join(UPLOAD_PATH, form.image.data), 'w').write(image_data)
-
-
-
-
-#     def validate_username(self, username):
-#         user = User.query.filter_by(username=username.data).first()
-#         if user:
-#             raise ValidationError('That username is taken. Please choose another one')
-#     def validate_email(self, email):
-#         user = User.query.filter_by(email=email.data).first()
-#         if user:
-#             raise ValidationError('That email is taken. Please choose another one')
-
-# class LoginForm(FlaskForm):
-#     email = StringField('Email',
-#                         validators=[DataRequired(), Email()])
-#     password = PasswordField('Password', validators=[DataRequired()])
-#     remember = BooleanField('Remember Me')
-#     submit = SubmitField('Login')
--- sudokuApp/routes.py
+++ sudokuApp/routes.py
@@ -11,6 +11,7 @@ import cv2
 from PIL import Image
 from flask import render_template, url_for, flash, redirect, request, session
 from flask import Flask
+from flask import Markup
 from sudokuApp import app
 from sudokuApp.forms import gridForm, imageUpload
 import numpy as np
@@ -27,49 +28,17 @@ from sudokuApp.SudokuBoard import SudokuSolver
 #db = SQLAlchemy(app)
 #bcrypt = Bcrypt(app)
 
-grid = np.zeros((9,9), dtype=np.int)
-        
-grid[0,0] = 8
-
-grid[1,2] = 3
-grid[1,3] = 6
-
-grid[2,1] = 7
-grid[2,4] = 9
-grid[2,6] = 2
-
-grid[3,1] = 5
-grid[3,5] = 7
-
-grid[4,4] = 4
-grid[4,5] = 5
-grid[4,6] = 7
-
-grid[5,3] = 1
-grid[5,7] = 3
-
-grid[6,2] = 1
-grid[6,7] = 6
-grid[6,8] = 8
-
-grid[7,2] = 8
-grid[7,3] = 5
-grid[7,7] = 1
-
-grid[8,1] = 9
-grid[8,6] = 4
-
 
 def save_picture(form_picture):
     random_hex = secrets.token_hex(8)
     _, f_ext = os.path.splitext(form_picture.filename)
     picture_fn = random_hex + f_ext
     picture_path = os.path.join(app.root_path, 'static/puzzlePics', picture_fn)
-
-    output_size = (125, 125)
+    output_size = (500, 500)
     i = Image.open(form_picture)
     i.thumbnail(output_size)
     i.save(picture_path)
+    session['picPath'] = picture_fn
     return picture_fn
 
 def process_picture(form_picture):
@@ -82,14 +51,16 @@ def process_picture(form_picture):
 	imProcessor = sudokuImageProcessor(cvImage)
 	return imProcessor.getPredictedGrid()
 
-def solve_puzzle(gridVerified):
-	board = SudokuSolver(gridVerified)
-	return board.solve()
+# def solve_puzzle(gridVerified):
+# 	board = SudokuSolver(gridVerified)
+# 	return board.solve()
 
 @app.route("/", methods=['GET', 'POST'])
 @app.route("/home", methods=['GET', 'POST'])
 def home():
+	#create form
 	form = imageUpload()
+
 	if form.validate_on_submit():
 		if form.picture.data:
 			#save sudoku puzzle picture to local directory
@@ -100,16 +71,22 @@ def home():
 				session['procGrid'] = process_picture(form.picture.data).tolist()
 			except:
 				#if error occurs in image processing. reload page
-				flash(f'Grid was not recognized. Please try again', 'danger')
+				flash(Markup('Error! A Sudoku grid was not found in the image. Please try again or enter the digits manually <a href="/verify" class="alert-link">here</a>'), 'danger')
+				#clear grid from previous sessions
+				session['procGrid'] = np.zeros((9,9)).tolist()
 				return redirect(url_for('home'))
 			return redirect(url_for('verify'))
 		else:	#if no picture was uploaded use default
+			#put picture file path as default
+			session['picPath'] = 'default.jpg'
 			try:
 				#no picture selected. use default
 				session['procGrid'] = process_picture('sudokuApp\\static\\siteImages\\default.jpg').tolist()
 			except:
 				#if error occurs in image processing. reload page
-				flash(f'Grid was not recognized. Please try again', 'danger')
+				flash(Markup('Error! A Sudoku grid was not found in the image. Please try again or enter the digits manually <a href="/verify" class="alert-link">here</a>'), 'danger')
+				#clear grid from previous sessions
+				session['procGrid'] = np.zeros((9,9)).tolist()
 				return redirect(url_for('home'))
 			return redirect(url_for('verify'))
 	return render_template('imageUpload.html', form=form) 
@@ -118,28 +95,34 @@ def home():
 @app.route("/verify", methods=['GET', 'POST'])
 def verify():
 	#retrieve the grid inputs passed through the session cookie
-	gridOut = np.array(session.pop('procGrid', None))
-
+	gridOut = np.array(session.get('procGrid', 'not set'))
+	#get puzzle picture path for display
+	picPath =  url_for('static', filename='puzzlePics/' + session.get('picPath', 'not set'))
+	#create flask form
 	form = gridForm()
 	if form.validate_on_submit():
 		#gather results from form and convert None entries to 0
 		gridVerified = [field.data if field.data else 0 for field in form.cellVals.entries]
 		gridVerified = np.array(gridVerified).reshape((9,9)) #reformat grid to be fed into solver
 		#call function to solve puzzle
-		session['solution'] = solve_puzzle(gridVerified).tolist()
+		board = SudokuSolver(gridVerified)
+		try:
+			#attempt to solve
+			session['solution'] = board.solve().tolist()
+			#flash(f'Sudoku Board Entries have been confirmed. Solving puzzle...', 'success')
+			return redirect(url_for('solution'))
+		except:   #if there is an exception give use option to correct grid or values found
+			session['solution'] = board.getGrid().tolist()
+			flash(Markup('Error! The program was not able to find a solution. Please recheck entries. If all entries were correct, click <a href="/solution" class="alert-link">here</a> to see partial solution.'), 'danger')
+			return redirect(url_for('verify'))
 
-		#flash(f'Sudoku Board Entries have been confirmed. Solving puzzle...', 'success')
-		return redirect(url_for('solution'))
-	return render_template('verifyPuzzle.html', form=form, grid=gridOut.flatten())
+	return render_template('verifyPuzzle.html', form=form, grid=gridOut.flatten(), imagePath=picPath)
 
 
 
 @app.route("/solution", methods=['GET', 'POST'])
 def solution():
-	#gridOut = request.args['inputGrid'] used when arguments passed over request
-	#gridOut = session['confGrid']
 	gridOut = np.array(session.pop('solution', None)).flatten()
-	#form = populateGridForm()
 	form = gridForm()
 	if form.validate_on_submit():
 		#gather results from form and convert None entries to 0
@@ -151,53 +134,3 @@ def solution():
 def about():
 	return render_template('about.html')
 
-
-
-
-# @app.route("/", methods=['GET', 'POST'])
-# @app.route("/home", methods=['GET', 'POST'])
-# def home():
-#     form = gridForm()
-#     if form.validate_on_submit():
-#     	#gather results from form and convert None entries to 0
-#     	gridOut = [field.data if field.data else 0 for field in form.cellVals.entries]
-#     	session['confGrid'] = gridOut
-#     	#if successful submission, send success message
-#     	flash(f'Sudoku Board Entries have been confirmed. Solving puzzle...', 'success')
-#     	return redirect(url_for('solver'))
-#     return render_template('home.html', form=form, grid=grid.flatten())
- 
-
-# @app.route("/submitImage", methods=['GET', 'POST'])
-# def submitImage():
-# 	form = imageUpload()
-# 	if form.validate_on_submit():
-# 		#gather results from form and convert None entries to 0
-# 		flash(f'Upload complete. Digit recognition in progress. Please wait...', 'success')
-# 	return render_template('imageUpload.html', form=form)
-
-
-
-# @app.route("/account", methods=['GET', 'POST'])
-# @login_required
-# def account():
-#     form = UpdateAccountForm()
-#     if form.validate_on_submit():
-#         if form.picture.data:
-#             picture_file = save_picture(form.picture.data)
-#             current_user.image_file = picture_file
-#         current_user.username = form.username.data
-#         current_user.email = form.email.data
-#         db.session.commit()
-#         flash('Your account has been updated!', 'success')
-#         return redirect(url_for('account'))
-#     elif request.method == 'GET':
-#         form.username.data = current_user.username
-#         form.email.data = current_user.email
-#     image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
-#     return render_template('account.html', title='Account',
-#                            image_file=image_file, form=form)
-
-
-# if __name__ == '__main__':
-#     app.run(debug=True)
